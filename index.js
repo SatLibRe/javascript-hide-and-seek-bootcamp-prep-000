f1bffdc1ec938925aad8fb69f84f0651567e6def
free-writer-5650 <miles.higbie@gmail.com>
2019-10-13 16:45:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function deepestChild(){
   return deep[deep.length-1]
 }
 
-function increaseRankBy(n) {
-  
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+
+  for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    }
+
 }
